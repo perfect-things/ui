8376f966c6bcab8e98f9a3672ecafef880376ac5
Dziad Borowy <tborychowski@gmail.com>
2021-10-20 17:16:56 +0100

    better number

--- docs/components/input.svelte
+++ docs/components/input.svelte
@@ -9,11 +9,18 @@
 <h3>Number</h3>
 <input type="number" />
 
+<h3>Better Number</h3>
+<Number />
+
+<h3>Maths (allow +-*/)</h3>
+
+
 <h3>Search</h3>
 <input type="search" />
 
 <script>
 import '../../src/input';
+import Number from '../../src/input-number';
 
 function oninput (e) {
 	console.log(e);
---
+++ src/input-number/index.svelte
@@ -0,0 +1,36 @@
+<input type="text" bind:value="{value}" on:keydown="{onkeydown}" on:change="{onchange}">
+<script>
+let value = '';
+const DECIMAL_SEPARATOR = '.';
+const allowedKeys = [
+	'0',
+	'1',
+	'2',
+	'3',
+	'4',
+	'5',
+	'6',
+	'7',
+	'8',
+	'9',
+	'ArrowLeft',
+	'ArrowDown',
+	'ArrowUp',
+	'ArrowRight',
+	'Backspace',
+	'Delete',
+	'Tab',
+	'Meta',
+];
+
+function onkeydown (e) {
+	if (allowedKeys.includes(e.key)) return;
+	if (e.key === DECIMAL_SEPARATOR && !value.includes(DECIMAL_SEPARATOR)) return;
+	e.preventDefault();
+}
+
+function onchange () {
+	const num = parseFloat(value);
+	value = isNaN(num) ? '' : num;
+}
+</script>
