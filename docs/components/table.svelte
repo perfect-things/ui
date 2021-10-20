c96b3dbcc38f3f31229868dc67cd8302a2a05f49
Dziad Borowy <tborychowski@gmail.com>
2021-10-20 16:21:35 +0100

    table

--- docs/components/table.svelte
+++ docs/components/table.svelte
@@ -1,29 +1,100 @@
 <style>
-	.viewport { width: 50%; height: 50%; }
+	.viewport { width: 50%; height: 500px; border: 1px dashed red; }
 </style>
 <h2>Table</h2>
 
 <div class="viewport">
 	<Table>
 		<thead>
-			<tr><th>Month</th><th>Savings</th></tr>
+			<tr><th>Year</th><th>Month</th><th>Price</th></tr>
+		</thead>
+		<thead>
+			<tr><th colspan="3">Year: 2021</th></tr>
 		</thead>
 		<tbody>
-			<tr><td>January</td><td>$100</td></tr>
-			<tr><td>February</td><td>$80</td></tr>
-			<tr><td>March</td><td>$80</td></tr>
-			<tr><td>April</td><td>$80</td></tr>
-			<tr><td>May</td><td>$80</td></tr>
-			<tr><td>June</td><td>$80</td></tr>
-			<tr><td>July</td><td>$80</td></tr>
-			<tr><td>August</td><td>$80</td></tr>
-			<tr><td>September</td><td>$80</td></tr>
-			<tr><td>October</td><td>$80</td></tr>
-			<tr><td>November</td><td>$80</td></tr>
-			<tr><td>December</td><td>$80</td></tr>
+			<tr><td>2021</td><td>January</td><td>$100</td></tr>
+			<tr><td>2021</td><td>February</td><td>$80</td></tr>
+			<tr><td>2021</td><td>March</td><td>$80</td></tr>
+			<tr><td>2021</td><td>April</td><td>$80</td></tr>
+			<tr><td>2021</td><td>May</td><td>$80</td></tr>
+			<tr><td>2021</td><td>June</td><td>$80</td></tr>
+			<tr><td>2021</td><td>July</td><td>$80</td></tr>
+			<tr><td>2021</td><td>August</td><td>$80</td></tr>
+			<tr><td>2021</td><td>September</td><td>$80</td></tr>
+			<tr><td>2021</td><td>October</td><td>$80</td></tr>
+			<tr><td>2021</td><td>November</td><td>$80</td></tr>
+			<tr><td>2021</td><td>December</td><td>$80</td></tr>
+		</tbody>
+		<thead>
+			<tr><th colspan="3">Year: 2020</th></tr>
+		</thead>
+		<tbody>
+			<tr><td>2020</td><td>January</td><td>$100</td></tr>
+			<tr><td>2020</td><td>February</td><td>$80</td></tr>
+			<tr><td>2020</td><td>March</td><td>$80</td></tr>
+			<tr><td>2020</td><td>April</td><td>$80</td></tr>
+			<tr><td>2020</td><td>May</td><td>$80</td></tr>
+			<tr><td>2020</td><td>June</td><td>$80</td></tr>
+			<tr><td>2020</td><td>July</td><td>$80</td></tr>
+			<tr><td>2020</td><td>August</td><td>$80</td></tr>
+			<tr><td>2020</td><td>September</td><td>$80</td></tr>
+			<tr><td>2020</td><td>October</td><td>$80</td></tr>
+			<tr><td>2020</td><td>November</td><td>$80</td></tr>
+			<tr><td>2020</td><td>December</td><td>$80</td></tr>
+		</tbody>
+		<thead>
+			<tr><th colspan="3">Year: 2019</th></tr>
+		</thead>
+		<tbody>
+			<tr><td>2019</td><td>January</td><td>$100</td></tr>
+			<tr><td>2019</td><td>February</td><td>$80</td></tr>
+			<tr><td>2019</td><td>March</td><td>$80</td></tr>
+			<tr><td>2019</td><td>April</td><td>$80</td></tr>
+			<tr><td>2019</td><td>May</td><td>$80</td></tr>
+			<tr><td>2019</td><td>June</td><td>$80</td></tr>
+			<tr><td>2019</td><td>July</td><td>$80</td></tr>
+			<tr><td>2019</td><td>August</td><td>$80</td></tr>
+			<tr><td>2019</td><td>September</td><td>$80</td></tr>
+			<tr><td>2019</td><td>October</td><td>$80</td></tr>
+			<tr><td>2019</td><td>November</td><td>$80</td></tr>
+			<tr><td>2019</td><td>December</td><td>$80</td></tr>
+		</tbody>
+		<thead>
+			<tr><th colspan="3">Year: 2018</th></tr>
+		</thead>
+		<tbody>
+			<tr><td>2018</td><td>January</td><td>$100</td></tr>
+			<tr><td>2018</td><td>February</td><td>$80</td></tr>
+			<tr><td>2018</td><td>March</td><td>$80</td></tr>
+			<tr><td>2018</td><td>April</td><td>$80</td></tr>
+			<tr><td>2018</td><td>May</td><td>$80</td></tr>
+			<tr><td>2018</td><td>June</td><td>$80</td></tr>
+			<tr><td>2018</td><td>July</td><td>$80</td></tr>
+			<tr><td>2018</td><td>August</td><td>$80</td></tr>
+			<tr><td>2018</td><td>September</td><td>$80</td></tr>
+			<tr><td>2018</td><td>October</td><td>$80</td></tr>
+			<tr><td>2018</td><td>November</td><td>$80</td></tr>
+			<tr><td>2018</td><td>December</td><td>$80</td></tr>
+		</tbody>
+		<thead>
+			<tr><th colspan="3">Year: 2017</th></tr>
+		</thead>
+		<tbody>
+			<tr><td>2017</td><td>January</td><td>$100</td></tr>
+			<tr><td>2017</td><td>February</td><td>$80</td></tr>
+			<tr><td>2017</td><td>March</td><td>$80</td></tr>
+			<tr><td>2017</td><td>April</td><td>$80</td></tr>
+			<tr><td>2017</td><td>May</td><td>$80</td></tr>
+			<tr><td>2017</td><td>June</td><td>$80</td></tr>
+			<tr><td>2017</td><td>July</td><td>$80</td></tr>
+			<tr><td>2017</td><td>August</td><td>$80</td></tr>
+			<tr><td>2017</td><td>September</td><td>$80</td></tr>
+			<tr><td>2017</td><td>October</td><td>$80</td></tr>
+			<tr><td>2017</td><td>November</td><td>$80</td></tr>
+			<tr><td>2017</td><td>December</td><td>$80</td></tr>
 		</tbody>
 		<tfoot>
-			<tr><td>Sum</td><td>$180</td></tr>
+			<tr><td colspan="2">Sum</td><td>$180</td></tr>
 		</tfoot>
 	</Table>
 </div>
