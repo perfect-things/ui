0e33e772e21b7b77240f771bd0890ae208edc41a
Tomasz Borychowski <github@borychowski.org>
2021-12-23 14:38:51 +0000

    improve accordion

--- src/panel/index.svelte
+++ src/panel/index.svelte
@@ -1,16 +1,17 @@
-<div class="panel" class:collapsed>
-	<h2 class="panel-header" tabindex="0"
-		on:keypress={toggle}
-		on:click|preventDefault={toggle}>
+<details class="panel" class:collapsed {open}
+	on:keypress={toggle}
+	on:click|preventDefault={toggle}>
+
+	<summary class="panel-header">
 		{title}
 		<div class="chevron"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="6 9 12 15 18 9"/></svg></div>
-	</h2>
+	</summary>
 	{#if !collapsed}
 		<div class="panel-content" transition:slide|local="{{ duration: 300 }}">
 			<slot></slot>
 		</div>
 	{/if}
-</div>
+</details>
 
 
 <script>
@@ -18,10 +19,15 @@ import './index.css';
 import { slide } from 'svelte/transition';
 export let title = '';
 export let collapsed = false;
+let open = !collapsed;
+let timer;
 
 function toggle (e) {
 	if (e.type === 'keypress' && e.key !== 'Enter' && e.key !== ' ') return;
 	collapsed = !collapsed;
+	if (timer) clearTimeout(timer);
+	if (collapsed) timer = setTimeout(() => open = false, 300);
+	else open = true;
 }
 
 </script>
