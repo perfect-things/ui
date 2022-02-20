074083f0632701564857589f28fbb7f24755a1b8
Tomasz Borychowski <github@borychowski.org>
2022-02-20 22:12:32 +0000

    twinkl

--- docs/components/dialog.svelte
+++ docs/components/dialog.svelte
@@ -3,7 +3,9 @@
 </style>
 <h2>Dialog</h2>
 
-<Button on:click="{() => dialog.open()}">Show dialog</Button>
+<Button on:click="{() => dialog1.open()}">Show dialog 1</Button>
+<Button on:click="{() => dialog2.open()}">Show dialog 2</Button>
+<Button on:click="{() => dialog3.open()}">Show dialog 3</Button>
 
 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris mollis porttitor justo, eget ornare massa commodo non. Pellentesque semper dictum mauris, id pretium mi mattis in. Proin sodales neque id euismod interdum. Fusce vel blandit orci. Mauris nec ligula aliquam, vestibulum erat nec, ullamcorper nunc. Cras vel lacinia sem. Aenean non tincidunt nisl, vitae consectetur est. Integer id neque tempor, facilisis felis egestas, aliquam turpis. Mauris id consectetur purus. Praesent vehicula, mauris eu hendrerit vehicula, velit tortor fermentum enim, eget malesuada quam eros at quam. Integer mattis egestas tempus.</p>
 <p>Aliquam et purus enim. Suspendisse potenti. Suspendisse tincidunt ullamcorper nulla non gravida. Morbi at tellus dui. Sed orci ligula, facilisis sit amet odio eu, commodo ultricies lorem. Nullam sagittis sapien metus, eu posuere sem iaculis sed. Duis at nibh feugiat, placerat lectus nec, consectetur elit. In sollicitudin est in ultricies gravida. Ut malesuada ex lacinia, posuere augue eget, imperdiet erat. Phasellus ac dui sit amet ligula condimentum venenatis vitae ornare augue. Vivamus pellentesque felis in orci finibus, a accumsan libero consectetur.</p>
@@ -11,7 +13,7 @@
 <p>Suspendisse sollicitudin sed ligula nec tempus. Phasellus quis luctus sapien. Nullam nec sapien fringilla, sollicitudin dui sit amet, molestie arcu. Pellentesque id elit et sem pharetra gravida. Donec sed metus ut dui venenatis euismod varius ut libero. Duis ornare odio finibus eros rhoncus ullamcorper. Maecenas auctor lectus volutpat sem pretium volutpat. Mauris blandit quam diam, nec consequat arcu dignissim ut. Donec ac lacus pretium, sollicitudin nisi in, ullamcorper enim. Ut convallis nec eros nec scelerisque. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Mauris non odio a ipsum varius pretium non ut ex. Quisque euismod luctus risus, sit amet venenatis justo vehicula non. Aliquam erat volutpat. Phasellus eu leo ut odio cursus cursus. Pellentesque porta odio id arcu mattis, vitae aliquam risus efficitur.</p>
 <p>Curabitur nec cursus purus. Nullam scelerisque et odio ut pretium. Donec gravida auctor enim, in venenatis mi viverra sit amet. Integer tincidunt lectus quis sagittis pellentesque. Morbi nec ipsum erat. Donec finibus sit amet lorem et dignissim. Praesent pretium consequat enim, quis rutrum nisl imperdiet ut. </p>
 
-<Dialog bind:this="{dialog}" title="Hello">
+<Dialog bind:this="{dialog1}" title="Hello">
 	dialog contents<br>
 	Hello world!
 
@@ -22,13 +24,29 @@
 	<p>Curabitur nec cursus purus. Nullam scelerisque et odio ut pretium. Donec gravida auctor enim, in venenatis mi viverra sit amet. Integer tincidunt lectus quis sagittis pellentesque. Morbi nec ipsum erat. Donec finibus sit amet lorem et dignissim. Praesent pretium consequat enim, quis rutrum nisl imperdiet ut. </p>
 
 	<div class="buttons">
-		<Button class="button" on:click="{() => dialog.close()}">Close</Button>
+		<Button class="button" on:click="{() => dialog1.close()}">Close</Button>
+	</div>
+</Dialog>
+
+<Dialog bind:this="{dialog2}" title="Hello">
+	Hello!
+	<div class="buttons">
+		<Button class="button" on:click="{() => dialog2.close()}">Close</Button>
+	</div>
+</Dialog>
+
+
+<Dialog bind:this="{dialog3}">
+	Are you sure?
+	<div class="buttons">
+		<Button class="button" on:click="{() => dialog3.close()}">Yes</Button>
+		<Button class="button" on:click="{() => dialog3.close()}">No</Button>
 	</div>
 </Dialog>
 
 
 <script>
 import { Button, Dialog } from '../../src';
-let dialog;
+let dialog1, dialog2, dialog3;
 
 </script>
