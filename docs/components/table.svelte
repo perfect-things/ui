92ca025f49d5cd2ea60e05878fa364b22c83e9ad
Tomasz Borychowski <github@borychowski.org>
2022-03-06 15:00:15 +0000

    selec-table-table

--- docs/components/table.svelte
+++ docs/components/table.svelte
@@ -1,7 +1,10 @@
 <h2>Table</h2>
 
 <div class="table-viewport">
-	<Table>
+	<Table rowSelector=".row-sel"
+		on:key="{onKey}"
+		on:dblclick="{onSelect}"
+		on:select="{onSelect}">
 		<thead>
 			<tr><th>Year</th><th>Month</th><th>Price</th></tr>
 		</thead>
@@ -9,93 +12,93 @@
 			<tr><th colspan="3">Year: 2021</th></tr>
 		</thead>
 		<tbody>
-			<tr><td>2021</td><td>January</td><td>$100</td></tr>
-			<tr><td>2021</td><td>February</td><td>$80</td></tr>
-			<tr><td>2021</td><td>March</td><td>$80</td></tr>
-			<tr><td>2021</td><td>April</td><td>$80</td></tr>
-			<tr><td>2021</td><td>May</td><td>$80</td></tr>
-			<tr><td>2021</td><td>June</td><td>$80</td></tr>
+			<tr class="row-sel"><td>2021</td><td>January</td><td>$100</td></tr>
+			<tr class="row-sel"><td>2021</td><td>February</td><td>$80</td></tr>
+			<tr class="row-sel"><td>2021</td><td>March</td><td>$80</td></tr>
+			<tr class="row-sel"><td>2021</td><td>April</td><td>$80</td></tr>
+			<tr class="row-sel"><td>2021</td><td>May</td><td>$80</td></tr>
+			<tr class="row-sel"><td>2021</td><td>June</td><td>$80</td></tr>
 		</tbody>
-		<tbody class="row-group">
+		<tbody class="row-sel row-group">
 			<tr><td>2021</td><td>April 1</td><td>$80</td></tr>
 			<tr><td>2021</td><td>April 2</td><td>$80</td></tr>
 		</tbody>
 		<tbody>
-			<tr><td>2021</td><td>May</td><td>$80</td></tr>
-			<tr><td>2021</td><td>July</td><td>$80</td></tr>
-			<tr><td>2021</td><td>August</td><td>$80</td></tr>
-			<tr><td>2021</td><td>September</td><td>$80</td></tr>
-			<tr><td>2021</td><td>October</td><td>$80</td></tr>
-			<tr><td>2021</td><td>November</td><td>$80</td></tr>
-			<tr><td>2021</td><td>December</td><td>$80</td></tr>
+			<tr class="row-sel"><td>2021</td><td>May</td><td>$80</td></tr>
+			<tr class="row-sel"><td>2021</td><td>July</td><td>$80</td></tr>
+			<tr class="row-sel"><td>2021</td><td>August</td><td>$80</td></tr>
+			<tr class="row-sel"><td>2021</td><td>September</td><td>$80</td></tr>
+			<tr class="row-sel"><td>2021</td><td>October</td><td>$80</td></tr>
+			<tr class="row-sel"><td>2021</td><td>November</td><td>$80</td></tr>
+			<tr class="row-sel"><td>2021</td><td>December</td><td>$80</td></tr>
 		</tbody>
 		<thead>
 			<tr><th colspan="3">Year: 2020</th></tr>
 		</thead>
 		<tbody>
-			<tr><td>2020</td><td>January</td><td>$100</td></tr>
-			<tr><td>2020</td><td>February</td><td>$80</td></tr>
-			<tr><td>2020</td><td>March</td><td>$80</td></tr>
-			<tr><td>2020</td><td>April</td><td>$80</td></tr>
-			<tr><td>2020</td><td>May</td><td>$80</td></tr>
-			<tr><td>2020</td><td>June</td><td>$80</td></tr>
-			<tr><td>2020</td><td>July</td><td>$80</td></tr>
-			<tr><td>2020</td><td>August</td><td>$80</td></tr>
-			<tr><td>2020</td><td>September</td><td>$80</td></tr>
-			<tr><td>2020</td><td>October</td><td>$80</td></tr>
-			<tr><td>2020</td><td>November</td><td>$80</td></tr>
-			<tr><td>2020</td><td>December</td><td>$80</td></tr>
+			<tr class="row-sel"><td>2020</td><td>January</td><td>$100</td></tr>
+			<tr class="row-sel"><td>2020</td><td>February</td><td>$80</td></tr>
+			<tr class="row-sel"><td>2020</td><td>March</td><td>$80</td></tr>
+			<tr class="row-sel"><td>2020</td><td>April</td><td>$80</td></tr>
+			<tr class="row-sel"><td>2020</td><td>May</td><td>$80</td></tr>
+			<tr class="row-sel"><td>2020</td><td>June</td><td>$80</td></tr>
+			<tr class="row-sel"><td>2020</td><td>July</td><td>$80</td></tr>
+			<tr class="row-sel"><td>2020</td><td>August</td><td>$80</td></tr>
+			<tr class="row-sel"><td>2020</td><td>September</td><td>$80</td></tr>
+			<tr class="row-sel"><td>2020</td><td>October</td><td>$80</td></tr>
+			<tr class="row-sel"><td>2020</td><td>November</td><td>$80</td></tr>
+			<tr class="row-sel"><td>2020</td><td>December</td><td>$80</td></tr>
 		</tbody>
 		<thead>
 			<tr><th colspan="3">Year: 2019</th></tr>
 		</thead>
 		<tbody>
-			<tr><td>2019</td><td>January</td><td>$100</td></tr>
-			<tr><td>2019</td><td>February</td><td>$80</td></tr>
-			<tr><td>2019</td><td>March</td><td>$80</td></tr>
-			<tr><td>2019</td><td>April</td><td>$80</td></tr>
-			<tr><td>2019</td><td>May</td><td>$80</td></tr>
-			<tr><td>2019</td><td>June</td><td>$80</td></tr>
-			<tr><td>2019</td><td>July</td><td>$80</td></tr>
-			<tr><td>2019</td><td>August</td><td>$80</td></tr>
-			<tr><td>2019</td><td>September</td><td>$80</td></tr>
-			<tr><td>2019</td><td>October</td><td>$80</td></tr>
-			<tr><td>2019</td><td>November</td><td>$80</td></tr>
-			<tr><td>2019</td><td>December</td><td>$80</td></tr>
+			<tr class="row-sel"><td>2019</td><td>January</td><td>$100</td></tr>
+			<tr class="row-sel"><td>2019</td><td>February</td><td>$80</td></tr>
+			<tr class="row-sel"><td>2019</td><td>March</td><td>$80</td></tr>
+			<tr class="row-sel"><td>2019</td><td>April</td><td>$80</td></tr>
+			<tr class="row-sel"><td>2019</td><td>May</td><td>$80</td></tr>
+			<tr class="row-sel"><td>2019</td><td>June</td><td>$80</td></tr>
+			<tr class="row-sel"><td>2019</td><td>July</td><td>$80</td></tr>
+			<tr class="row-sel"><td>2019</td><td>August</td><td>$80</td></tr>
+			<tr class="row-sel"><td>2019</td><td>September</td><td>$80</td></tr>
+			<tr class="row-sel"><td>2019</td><td>October</td><td>$80</td></tr>
+			<tr class="row-sel"><td>2019</td><td>November</td><td>$80</td></tr>
+			<tr class="row-sel"><td>2019</td><td>December</td><td>$80</td></tr>
 		</tbody>
 		<thead>
 			<tr><th colspan="3">Year: 2018</th></tr>
 		</thead>
 		<tbody>
-			<tr><td>2018</td><td>January</td><td>$100</td></tr>
-			<tr><td>2018</td><td>February</td><td>$80</td></tr>
-			<tr><td>2018</td><td>March</td><td>$80</td></tr>
-			<tr><td>2018</td><td>April</td><td>$80</td></tr>
-			<tr><td>2018</td><td>May</td><td>$80</td></tr>
-			<tr><td>2018</td><td>June</td><td>$80</td></tr>
-			<tr><td>2018</td><td>July</td><td>$80</td></tr>
-			<tr><td>2018</td><td>August</td><td>$80</td></tr>
-			<tr><td>2018</td><td>September</td><td>$80</td></tr>
-			<tr><td>2018</td><td>October</td><td>$80</td></tr>
-			<tr><td>2018</td><td>November</td><td>$80</td></tr>
-			<tr><td>2018</td><td>December</td><td>$80</td></tr>
+			<tr class="row-sel"><td>2018</td><td>January</td><td>$100</td></tr>
+			<tr class="row-sel"><td>2018</td><td>February</td><td>$80</td></tr>
+			<tr class="row-sel"><td>2018</td><td>March</td><td>$80</td></tr>
+			<tr class="row-sel"><td>2018</td><td>April</td><td>$80</td></tr>
+			<tr class="row-sel"><td>2018</td><td>May</td><td>$80</td></tr>
+			<tr class="row-sel"><td>2018</td><td>June</td><td>$80</td></tr>
+			<tr class="row-sel"><td>2018</td><td>July</td><td>$80</td></tr>
+			<tr class="row-sel"><td>2018</td><td>August</td><td>$80</td></tr>
+			<tr class="row-sel"><td>2018</td><td>September</td><td>$80</td></tr>
+			<tr class="row-sel"><td>2018</td><td>October</td><td>$80</td></tr>
+			<tr class="row-sel"><td>2018</td><td>November</td><td>$80</td></tr>
+			<tr class="row-sel"><td>2018</td><td>December</td><td>$80</td></tr>
 		</tbody>
 		<thead>
 			<tr><th colspan="3">Year: 2017</th></tr>
 		</thead>
 		<tbody>
-			<tr><td>2017</td><td>January</td><td>$100</td></tr>
-			<tr><td>2017</td><td>February</td><td>$80</td></tr>
-			<tr><td>2017</td><td>March</td><td>$80</td></tr>
-			<tr><td>2017</td><td>April</td><td>$80</td></tr>
-			<tr><td>2017</td><td>May</td><td>$80</td></tr>
-			<tr><td>2017</td><td>June</td><td>$80</td></tr>
-			<tr><td>2017</td><td>July</td><td>$80</td></tr>
-			<tr><td>2017</td><td>August</td><td>$80</td></tr>
-			<tr><td>2017</td><td>September</td><td>$80</td></tr>
-			<tr><td>2017</td><td>October</td><td>$80</td></tr>
-			<tr><td>2017</td><td>November</td><td>$80</td></tr>
-			<tr><td>2017</td><td>December</td><td>$80</td></tr>
+			<tr class="row-sel"><td>2017</td><td>January</td><td>$100</td></tr>
+			<tr class="row-sel"><td>2017</td><td>February</td><td>$80</td></tr>
+			<tr class="row-sel"><td>2017</td><td>March</td><td>$80</td></tr>
+			<tr class="row-sel"><td>2017</td><td>April</td><td>$80</td></tr>
+			<tr class="row-sel"><td>2017</td><td>May</td><td>$80</td></tr>
+			<tr class="row-sel"><td>2017</td><td>June</td><td>$80</td></tr>
+			<tr class="row-sel"><td>2017</td><td>July</td><td>$80</td></tr>
+			<tr class="row-sel"><td>2017</td><td>August</td><td>$80</td></tr>
+			<tr class="row-sel"><td>2017</td><td>September</td><td>$80</td></tr>
+			<tr class="row-sel"><td>2017</td><td>October</td><td>$80</td></tr>
+			<tr class="row-sel"><td>2017</td><td>November</td><td>$80</td></tr>
+			<tr class="row-sel"><td>2017</td><td>December</td><td>$80</td></tr>
 		</tbody>
 		<tfoot>
 			<tr><td colspan="2">Sum</td><td>$180</td></tr>
@@ -106,4 +109,14 @@
 <script>
 import { Table } from '../../src';
 
+
+function onKey (e) {
+	const { event, selectedItem } = e.detail;
+	if (event.key === 'Enter') console.log(selectedItem);
+}
+
+function onSelect (e) {
+	const { selectedItem } = e.detail;
+	console.log(e.type, selectedItem);
+}
 </script>
--- src/table/index.svelte
+++ src/table/index.svelte
@@ -1,8 +1,130 @@
-<div class="table-wrapper {cssClass}" bind:this="{node}">
+<div class="table-wrapper {cssClass}"
+	class:selectable
+	bind:this="{node}"
+	on:click="{onClick}"
+	on:dblclick="{onDblClick}">
+
 	<table class="table"><slot /></table>
 </div>
 
 <script>
+import { onDestroy, onMount, createEventDispatcher } from 'svelte';
+const dispatch = createEventDispatcher();
+
 export let cssClass = '';
 export let node = undefined;
+export let selectable = true;
+// useful for when row-groups are needed.
+// then tbody.row-selector can be set to allow highlighting whole groups
+export let rowSelector = 'tbody tr';
+let selectedIdx = -1;
+let headerAndFooterHeight = 0;
+let clickTimer;
+
+
+onMount(() => {
+	if (selectable) {
+		document.addEventListener('keydown', onKeyDown);
+		makeRowsSelectable();
+		measureHeaderAndFooter();
+	}
+});
+
+onDestroy(() => {
+	if (selectable) {
+		document.removeEventListener('keydown', onKeyDown);
+		makeRowsNotSelectable();
+	}
+});
+
+function getSelectableItems () {
+	const rows = node.querySelectorAll(rowSelector);
+	if (rows && rows.length) return Array.from(rows);
+	return [];
+}
+
+function makeRowsSelectable () {
+	getSelectableItems().forEach(item => item.setAttribute('tabindex', 0));
+}
+
+function makeRowsNotSelectable () {
+	getSelectableItems().forEach(item => item.removeAttribute('tabindex'));
+}
+
+function selectPrev (skipEvent = false) {
+	const rows = getSelectableItems();
+	if (selectedIdx <= 0) return;
+	selectedIdx -= 1;
+	const rowEl = rows[selectedIdx];
+
+	const top = rowEl.offsetTop - headerAndFooterHeight;
+	if (node.scrollTop > top) node.scrollTo({ top, behavior: 'smooth' });
+	rowEl.focus();
+	if (!skipEvent) dispatch('select', { selectedItem: rowEl });
+}
+
+function selectNext (skipEvent = false) {
+	const rows = getSelectableItems();
+	if (selectedIdx >= rows.length - 1) return;
+	selectedIdx += 1;
+	const rowEl = rows[selectedIdx];
+
+	const top = rowEl.offsetTop + rowEl.offsetHeight - node.offsetHeight + headerAndFooterHeight;
+	if (node.scrollTop < top) node.scrollTo({ top, behavior: 'smooth' });
+	rowEl.focus();
+	if (!skipEvent) dispatch('select', { selectedItem: rowEl });
+}
+
+
+function measureHeaderAndFooter () {
+	requestAnimationFrame(() => {
+		headerAndFooterHeight = node.querySelector('thead').offsetHeight;
+	});
+}
+
+function onClick (e) {
+	if (clickTimer) clearTimeout(clickTimer);
+	clickTimer = setTimeout(() => _onClick(e), 200);
+}
+
+function _onClick (e) {
+	const row = e.target.closest(rowSelector);
+	if (!row) return;
+	const rows = getSelectableItems();
+	const idx = rows.findIndex(item => item == row);
+	if (idx < rows.length) {
+		selectedIdx = idx - 1;
+		selectNext(true);
+	}
+	else if (idx >= 0) {
+		selectedIdx = idx + 1;
+		selectPrev(true);
+	}
+	const rowEl = getSelectableItems()[selectedIdx];
+	dispatch('select', { selectedItem: rowEl });
+}
+
+function onDblClick (e) {
+	if (clickTimer) clearTimeout(clickTimer);
+	_onClick(e);
+	requestAnimationFrame(() => {
+		const selectedItem = getSelectableItems()[selectedIdx];
+		dispatch('dblclick', { selectedItem });
+	});
+}
+
+
+function onKeyDown (e) {
+	if (e.key === 'ArrowUp' || e.key === 'k') {
+		e.preventDefault();
+		selectPrev();
+	}
+	if (e.key === 'ArrowDown' || e.key === 'j') {
+		e.preventDefault();
+		selectNext();
+	}
+	const selectedItem = getSelectableItems()[selectedIdx];
+	dispatch('key', { event: e, selectedItem });
+}
+
 </script>
