1ca88b006e4d89e58777e51364c6fe41e0bbdc1e
Tomasz Borychowski <github@borychowski.org>
2022-03-12 15:22:25 +0000

    menu tweaks

--- docs/components/menu.svelte
+++ docs/components/menu.svelte
@@ -1,7 +1,26 @@
 <h2>Menu</h2>
 
 <h3>Normal menu</h3>
-<Button on:click="{menu0.open}">Show menu</Button>
+<Button on:click="{thingsMenu.open}">Show menu</Button>
+<Menu bind:this="{thingsMenu}">
+	<Item><Icon name="plus"/> Add a thing</Item>
+	<Item><Icon name=""/> Add another one</Item>
+	<Separator />
+	<Item on:click="{menuCloseThings}"><Icon name="close"/> {closeThingsText}</Item>
+</Menu>
+
+
+<h3>In a container with <em>overflow: hidden</em></h3>
+<div class="docs-menu-overflow">
+	<small>overflow: hidden</small>
+	<Button on:click="{someMenu.open}">Show menu</Button>
+	<Menu bind:this="{someMenu}">
+		<Item><Icon name="plus"/> Add some</Item>
+		<Item><Icon name=""/> Add some more</Item>
+		<Separator />
+		<Item><Icon name="close"/> Close something</Item>
+	</Menu>
+</div>
 
 
 <h3>Context menu</h3>
@@ -10,21 +29,14 @@
 <div class="div div1">Tab</div>
 <div class="div div2">Window</div>
 
-<Menu bind:this="{menu0}">
-	<Item on:click="{newTab}"><Icon name="plus"/> New Tab</Item>
-	<Item on:click="{newPrivateTab}"><Icon name=""/> New Private Tab</Item>
-	<Separator />
-	<Item on:click="{Menu0closeTabs}"><Icon name="close"/> {closeTabsText}</Item>
-</Menu>
-
-<Menu type="context" targetSelector=".div1" bind:this="{menu1}" on:close="{onMenu1close}">
+<Menu type="context" targetSelector=".div1" bind:this="{tabsMenu}" on:close="{onTabsMenuClose}">
 	<Item on:click="{newTab}"><Icon name="plus"/> New Tab</Item>
 	<Item on:click="{newPrivateTab}"><Icon name=""/> New Private Tab</Item>
 	<Separator />
 	<Item on:click="{closeTabs}"><Icon name="close"/> {closeTabsText}</Item>
 </Menu>
 
-<Menu type="context" targetSelector=".div2" bind:this="{menu2}">
+<Menu type="context" targetSelector=".div2" bind:this="{windowsMenu}">
 	<Item on:click="{newWindow}">New window</Item>
 	<Item on:click="{newPrivateWindow}">New private window</Item>
 	<Separator />
@@ -33,32 +45,38 @@
 
 <script>
 import { Button, Menu, Item, Separator, Icon } from '../../src';
-let menu0, menu1, menu2;
+let someMenu, thingsMenu, tabsMenu, windowsMenu;
+let closeThingsText = 'Close all things';
 let closeTabsText = 'Close all tabs';
-let menu1timer;
+let thingsMenuTimer, tabsMenutimer;
 
 
-function newTab () {
-	menu1.close().then(() => alert('New Tab clicked'));
-}
-function newPrivateTab () {
-	menu1.close().then(() => alert('New Private Tab clicked'));
-}
-
-function Menu0closeTabs (e) {
+function menuCloseThings (e) {
 	if (e && e.detail) e.detail.stopPropagation();
-	const initial = 'Close all tabs';
+	const initial = 'Close all things';
 	const confrm = 'Confirm Closing';
 
-	if (closeTabsText === initial) {
-		closeTabsText = confrm;
-		menu1timer = setTimeout(() => closeTabsText = initial, 2000);
+	if (closeThingsText === initial) {
+		closeThingsText = confrm;
+		thingsMenuTimer = setTimeout(() => closeThingsText = initial, 2000);
 	}
 	else {
-		menu0.close().then(() => alert('Closed all tabs!'));
+		thingsMenu.close().then(() => {
+			closeThingsText = initial;
+			if (thingsMenuTimer) clearTimeout(thingsMenuTimer);
+			alert('Closed all things!');
+		});
 	}
 }
 
+
+// TABS - menu2
+function newTab () {
+	tabsMenu.close().then(() => alert('New Tab clicked'));
+}
+function newPrivateTab () {
+	tabsMenu.close().then(() => alert('New Private Tab clicked'));
+}
 function closeTabs (e) {
 	if (e && e.detail) e.detail.stopPropagation();
 	const initial = 'Close all tabs';
@@ -66,26 +84,25 @@ function closeTabs (e) {
 
 	if (closeTabsText === initial) {
 		closeTabsText = confrm;
-		menu1timer = setTimeout(() => closeTabsText = initial, 2000);
+		tabsMenutimer = setTimeout(() => closeTabsText = initial, 2000);
 	}
-	else {
-		menu1.close().then(() => alert('Closed all tabs!'));
-	}
+	else tabsMenu.close().then(() => alert('Closed all tabs!'));
 }
-
-function onMenu1close () {
+function onTabsMenuClose () {
 	closeTabsText = 'Close all tabs';
-	if (menu1timer) clearTimeout(menu1timer);
+	if (tabsMenutimer) clearTimeout(tabsMenutimer);
 }
 
+
+// WINDOWS - menu3
 function newWindow () {
-	menu1.close().then(() => alert('New Window clicked'));
+	windowsMenu.close().then(() => alert('New Window clicked'));
 }
 function newPrivateWindow () {
-	// menu1.close().then(() => alert('New Private Window clicked'));
+	windowsMenu.close().then(() => alert('New Private Window clicked'));
 }
 function closeWindows () {
-	menu1.close().then(() => alert('Windows closed!'));
+	windowsMenu.close().then(() => alert('Windows closed!'));
 }
 
 
