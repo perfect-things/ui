f72499714025589753692a6d86d9b5b10557d144
Tomasz Borychowski <github@borychowski.org>
2022-03-22 22:53:28 +0000

    mo' colors

--- docs/components/color-palette.svelte
+++ docs/components/color-palette.svelte
@@ -1,17 +1,47 @@
 <h2>Color Palette</h2>
 
 <h3>Special colors</h3>
+
+<h4>Accent</h4>
 <div class="group">
 	<PaletteBox bg="color-accent" />
 	<PaletteBox bg="color-accent-semi" />
+	<PaletteBox bg="color-accent-light-1" text="color-border" />
+	<PaletteBox bg="color-accent-light-2" text="color-border" />
+	<PaletteBox bg="color-accent-dark-1" />
+	<PaletteBox bg="color-accent-dark-2" />
+</div>
+
+<h4>Info</h4>
+<div class="group">
 	<PaletteBox bg="color-info" />
 	<PaletteBox bg="color-info-semi" />
+	<PaletteBox bg="color-info-dark-1" />
+	<PaletteBox bg="color-info-dark-2" />
+</div>
+
+<h4>Success</h4>
+<div class="group">
 	<PaletteBox bg="color-success" />
 	<PaletteBox bg="color-success-semi" />
-	<PaletteBox bg="color-danger" />
-	<PaletteBox bg="color-danger-semi" />
+	<PaletteBox bg="color-success-dark-1" />
+	<PaletteBox bg="color-success-dark-2" />
+</div>
+
+<h4>Warning</h4>
+<div class="group">
 	<PaletteBox bg="color-warning" />
 	<PaletteBox bg="color-warning-semi" />
+	<PaletteBox bg="color-warning-dark-1" />
+	<PaletteBox bg="color-warning-dark-2" />
+</div>
+
+<h4>Danger</h4>
+<div class="group">
+	<PaletteBox bg="color-danger" />
+	<PaletteBox bg="color-danger-semi" />
+	<PaletteBox bg="color-danger-dark-1" />
+	<PaletteBox bg="color-danger-dark-2" />
 </div>
 
 <h3>Highlight colors</h3>
