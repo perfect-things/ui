33054e00a67e9a2c42f516815f60d66a3c6710ff
Dziad Borowy <github@borychowski.org>
2022-06-10 15:21:47 +0100

    toggle button style tweaks

--- docs/components/button-toggle.svelte
+++ docs/components/button-toggle.svelte
@@ -3,6 +3,11 @@
 <h3>Normal</h3>
 <ButtonToggle items="{buttons}" value="1" />
 
+<h3>Fit width</h3>
+<div class="button-toggle-wrapper-wide">
+	<ButtonToggle items="{buttons}" value="1" />
+</div>
+
 <br><br>
 
 <h4>Disabled</h4>
@@ -32,7 +37,7 @@ const buttons = [
 
 const items = [
 	{ name: 'One',   icon: 'info',  value: '1' },
-	{ name: 'Two',   icon: 'check', value: '2' },
+	{ name: 'Two and some long text',   icon: 'check', value: '2' },
 	{ name: 'Three', icon: 'alert', value: '3' },
 ];
 
