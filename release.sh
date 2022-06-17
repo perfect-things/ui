c2778a591e9b4a4564b5ed4398b0ac3df6b00767
Tomasz Borychowski <github@borychowski.org>
2022-06-17 20:59:33 +0100

    upgrade release script

--- release.sh
+++ release.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
+echo "0. don't bump"
+echo "1. major"
+echo "2. minor"
+echo "3. patch"
+echo "q. quit"
+read -p "Select version to publish (patch): " ans
+case $ans in
+    1  ) v=major ;;
+    2  ) v=minor ;;
+    q  ) v=patch ;;
+    "" ) v=patch ;;
+    *  ) echo "Whats that?" ;;
+esac
+
 npm run dist
 git add --all
-git commit -am 'bump'
-npm version patch
+git commit -am 'release'
+npm version $v
 git push
 npm publish
