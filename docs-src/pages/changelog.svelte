ede951fa757e9375a010b6f46ca7f491c002db33
Dziad Borowy <github@borychowski.org>
2023-03-24 22:08:41 +0000

    release

--- docs-src/pages/changelog.svelte
+++ docs-src/pages/changelog.svelte
@@ -1,4 +1,8 @@
 <h1 id="changelog">Changelog</h1>
+<h2 id="v627-2023-03-24">v6.2.7 <em>(2023-03-24)</em></h2>
+<ul>
+<li>revert some tooltip changes (<code>events</code> prop is actually useful)</li>
+</ul>
 <h2 id="v626-2023-03-24">v6.2.6 <em>(2023-03-24)</em></h2>
 <ul>
 <li>simplify tooltip (change bg color to <code>accent</code>, drop <code>events</code> prop and default to focus + hover)</li>
