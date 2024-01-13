b1f068d9a73983f5089eeff23f442070cdfc60ba
Dziad Borowy <github@borychowski.org>
2024-01-13 15:28:26 +0000

    round check icon tick

--- src/input/combobox/Combobox.svelte
+++ src/input/combobox/Combobox.svelte
@@ -91,7 +91,7 @@
 							on:touchend="{touchEnd}"
 							>
 							{#if multiselect}
-								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" class="icon icon-tabler icon-tabler-square-check">
+								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icon-tabler-square-check">
 									<rect x="4" y="4" width="16" height="16" rx="3"></rect>
 									<path class="tick" d="M8 12l3 3l5.5 -5.5"></path>
 								</svg>
