aad8b9e269c9c225739585ffc4001ea35eb4c02b
Dziad Borowy <github@borychowski.org>
2024-01-14 19:58:46 +0000

    dist

--- docs-src/pages/changelog.svelte
+++ docs-src/pages/changelog.svelte
@@ -1,5 +1,5 @@
 <h1>Changelog</h1>
-<h2>v9.4.3 <em>(2024-01-13)</em></h2>
+<h2>v9.4.3 <em>(2024-01-14)</em></h2>
 <ul>
 <li>More bugfixes, tests and some optimisations of the <code>Combobox</code> component.</li>
 </ul>
