cf33c160d2a9e372032fc5b1a9de35b74dcfa457
Dziad Borowy <github@borychowski.org>
2025-07-25 11:46:01 +0100

    tweak scripts

--- scripts/prerelease.sh
+++ scripts/prerelease.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 version=$(cat package.json | grep '"version"' | cut -d '"' -f 4)
+
+if [[ -z "$version" ]]; then
+	echo "Version not found. Please run this command in the project root directory."
+	exit 1
+fi
+
 echo "Current version: $version"
-echo "Enter - bump beta version and release"
-echo "c - use current version and release"
-echo "q - quit"
-read -p "Pre-release? " ans
+echo " 0  don't bump"
+echo " 1  bump beta version"
+echo " q  quit"
+echo ""
+read -p "Select version to publish (beta) " ans
 case $ans in
+	0  ) echo "Using current version" ;;
+	1  ) npm version prerelease --preid=beta ;;
 	"" ) npm version prerelease --preid=beta ;;
-	c | C | no | NO ) echo "Using current version" ;;
-    q  ) exit ;;
-    *  ) echo "What's that?" && exit ;;
+	q  ) exit ;;
+	*  ) echo "What's that?" && exit ;;
 esac
 
 version=$(cat package.json | grep '"version"' | cut -d '"' -f 4)
