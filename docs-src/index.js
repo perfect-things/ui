e5196e58ba99c14ef21576478f4275d082d6192c
Dziad Borowy <github@borychowski.org>
2025-11-27 16:08:23 +0000

    fix vite css build

--- docs-src/index.js
+++ docs-src/index.js
@@ -1,4 +1,9 @@
 import { mount } from 'svelte';
 import { App } from './app';
 
+
+import '../src/root.css';
+import '../src/theme-dark.css';
+import '../src/theme-light.css';
+
 export default mount(App, { target: document.getElementById('app'), });
